108b041cb1aa7f775ba92538e2b16165e3227af1
David Veszelovszki <veszelovszki@gmail.com>
2026-01-28 17:58:25 +0100

    Decrease min photos

--- src/lib/game/constants.ts
+++ src/lib/game/constants.ts
@@ -52,7 +52,7 @@ export const DEFAULT_ROUNDS = 12;
 export const MAX_PLAYERS = 8;
 
 /** Minimum number of photos required per player */
-export const MIN_PHOTOS_PER_PLAYER = 15;
+export const MIN_PHOTOS_PER_PLAYER = 3;
 
 /** Maximum number of photos the user can pick from Google Photos */
 export const MAX_PHOTOS_TO_PICK = 50;
